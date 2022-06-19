f4b843cb985ee9001177091e3b38e5cc411f90ac
Ayuan <838478249@qq.com>
2022-06-19 20:20:15 +0800

    kafkademo

--- kafka-app/src/main/java/com/lh/kafka/service/ProducerServiceImpl.java
+++ kafka-app/src/main/java/com/lh/kafka/service/ProducerServiceImpl.java
@@ -1,9 +1,13 @@
 package com.lh.kafka.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 /**
  * 生产者实现类
@@ -11,6 +15,7 @@ import org.springframework.util.StringUtils;
  * @author lh
  */
 @Service
+@Slf4j
 public class ProducerServiceImpl implements ProducerService {
 
     private static final String TOPIC_NAME = "demo";
@@ -24,6 +29,17 @@ public class ProducerServiceImpl implements ProducerService {
         if (!StringUtils.hasLength(idCard)) {
             return;
         }
-        kafkaTemplate.send(TOPIC_NAME, idCard);
+        ListenableFuture<SendResult<String, Object>> result = kafkaTemplate.send(TOPIC_NAME, idCard);
+        result.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("producer send faild: {}", ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("producer send success：{}", result.toString());
+            }
+        });
     }
 }
